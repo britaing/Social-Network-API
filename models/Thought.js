366a9e3d4795295a07cea7ccd383c7c623097825
Britain <britaing@gmail.com>
2023-10-18 13:20:21 -0500

    added thought model

---
+++ models/Thought.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: "string",
+    required: true,
+    maxLength: 280,
+    minLength: 1,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    //check on getter criteria
+  },
+  username: {
+    type: "string",
+    required: true,
+  },
+  reactions: [reactionSchema],
+},
+{
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  });
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+const Thought = model('thought', thoughtSchema);
+
+module.exports = Thought;
