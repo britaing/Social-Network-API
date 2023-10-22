cd97c8fde8b7a0f286ffff896397a5fe1b3d9e3e
Britain <britaing@gmail.com>
2023-10-22 13:29:34 -0500

    added index to routes and adjusted user api route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,14 @@
 const router = require('express').Router();
 
 // require controller
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    deleteUser,
+    updateUser,
+} = require('../../controllers/userController');
+
+router.route('/').get(getUsers).post(createUser);
 
 router.route('/').get()
